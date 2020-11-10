91a0a2b86859b3e8c43dfe479f903f994cc5f4e9
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-11-10 14:43:06 -0500

    add initialisers

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,87 @@ one constructor
 - use option constructor options
  */
 
-import { IBKRConnection } from '@stoqey/ibkr';
-import { FinnhubWS } from '@stoqey/finnhub';
+import { isEmpty, get as _get } from "lodash";
+import ibkr, { IBKRConnection, IbkrEvents } from "@stoqey/ibkr";
+import { FinnhubWS } from "@stoqey/finnhub";
 import { MarketDataProviders } from "./providers";
-
 
 /**
  * Hennessy provider
  */
- interface IHennessy {
-     provider?: MarketDataProviders;
-     env?: {
-        finnhubToken?: string;
-        ibkrPort?: number;
-        ibkrHost?: string;
-     }  
+interface IHennessy {
+  provider: MarketDataProviders; // or process.env.HEN_PROVIDER
+  env?: {
+    finnhubKey?: string; // or process.env.FINNHUB_KEY
+    ibPort?: number; // or process.env.IB_PORT
+    ibHost?: string; // or process.env.IB_PORT
+  };
 }
 
 /**
  * Hennessy APP
  */
 export class Hennessy {
+  provider: IbkrEvents | FinnhubWS | null = null;
 
-    provider: IBKRConnection | FinnhubWS | null =  null; 
-    constructor(args?: IHennessy) {
-        
-    }
+  constructor(args?: IHennessy) {
+    const startConfig = async (): Promise<void> => {
+      const { provider: providerType = MarketDataProviders.IBKR, env = null } =
+        args || {};
 
-    /**
-     * addSymbol
-     */
-    public addSymbol() {
-        
-    }
+      switch (providerType) {
+        case MarketDataProviders.FINNHUB:
+          const finnhubToken = _get(process.env, "FINNHUB_KEY", undefined);
+          this.provider = new FinnhubWS(finnhubToken);
 
-    /**
-     * removeSymbol
-     */
-    public removeSymbol() {
-        
-    }
+        //   All events here
+          break;
+        case MarketDataProviders.IBKR:
+          const ibProvider = IbkrEvents.Instance; // start ibkrEvents
 
-    /**
-     * switchToProvider
-     */
-    public switchToProvider(provider: MarketDataProviders) {
-        
-    }
+          const IB_HOST = _get(process.env, "IB_HOST", undefined);
+          const IB_PORT = _get(process.env, "IB_PORT", undefined);
+
+          if (IB_HOST && IB_PORT) {
+            const ib = await ibkr({
+              port: +IB_PORT,
+              host: IB_HOST,
+            });
+
+            if (!ib) {
+              throw new Error("error connecting to ibkr");
+            }
+
+            this.provider = ibProvider;
+
+            // TODO all events here
+          }
+          throw new Error("incorrect IB_PORT or IB_PORT, please try again");
+
+          break;
+        default:
+      }
+
+      return;
+    };
+
+    startConfig();
+  }
+
+  /**
+   * addSymbol
+   */
+  public addSymbol() {}
+
+  /**
+   * removeSymbol
+   */
+  public removeSymbol() {}
+
+  /**
+   * switchToProvider
+   */
+  public switchToProvider(provider: MarketDataProviders) {}
 }
 
 export default Hennessy;
