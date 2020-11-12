9dba918a8e240f6881a67f372037ad86476ab2d1
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-11-11 19:59:37 -0500

    get Quote

---
+++ src/Quota.ts
@@ -0,0 +1,65 @@
+/**
+ * Get quote prices
+ */
+import FinnhubAPI from "@stoqey/finnhub";
+import { ContractObject, HistoricalData } from "@stoqey/ibkr";
+import { isEmpty } from "lodash";
+import { log } from "./logs";
+
+type ContractSymbol = ContractObject | string;
+
+/**
+ * GetQuotePrice, use finnhub or IBKR
+ * @param contract
+ */
+export const GetQuotePrice = async (
+  contractSymbol: ContractSymbol,
+): Promise<number | undefined> => {
+  let closePrice: number | undefined;
+  const HistoricalDataManager = HistoricalData.Instance;
+  const symbol =
+    typeof contractSymbol === "string"
+      ? contractSymbol
+      : contractSymbol && contractSymbol.symbol;
+  const finnhubApi = new FinnhubAPI();
+  // 1. use Finnhub
+  const quote = await finnhubApi.getQuote(symbol);
+  closePrice = quote.close;
+  if (closePrice !== 0) {
+    log(
+      `Finnhub got closePrice symbol=${symbol} price=${closePrice} new entryPrice + marketPrice + averageCost`,
+      closePrice,
+    );
+  } else {
+    // 2. use interactive brokers from here
+    log(
+      `FINNHUB ${symbol} Failed to update portfolio market data empty entryPrice + marketPrice + averageCost`,
+      symbol,
+    );
+
+    const marketData = await HistoricalDataManager.reqHistoricalData({
+      symbol,
+      whatToShow: "TRADES",
+      durationStr: "1800 S",
+      barSizeSetting: "1 secs",
+    });
+
+    if (!isEmpty(marketData)) {
+      // use interactive brokers here
+      const lastItem = marketData.pop();
+      closePrice = lastItem && lastItem.close;
+
+      log(
+        `IBKR created closePrice ${symbol} price=${closePrice} new entryPrice + marketPrice + averageCost`,
+        closePrice,
+      );
+    } else {
+      log(
+        `IBKR ${symbol} Failed to update portfolio market data empty entryPrice + marketPrice + averageCost`,
+        symbol,
+      );
+    }
+  }
+
+  return closePrice;
+};
